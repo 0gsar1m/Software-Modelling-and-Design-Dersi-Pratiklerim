86d6380223bfdcc07f50755f10f5b001699684c6
0gsar1m <ogsarimemirkaan@gmail.com>
2026-03-16 02:00:14 +0300

    refac: Main class logic were fixed

--- src/creational_patterns/prototype/Color.java
+++ src/creational_patterns/prototype/Color.java
@@ -7,7 +7,7 @@ public abstract class Color implements Cloneable{
     public Object clone() {
         Object clone = null;
         try {
-            clone = super.clone();
+            clone = super.clone(); //
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
--- src/creational_patterns/prototype/Main.java
+++ src/creational_patterns/prototype/Main.java
@@ -6,8 +6,10 @@ public class Main {
                                                     // method will depend on a ColorStore object
 
         //If u define it static, Calling class like ColorStore.xxx below will be enough
-        Color c1 = ColorStore.getColorByName("blue");
+        Color copy1 = ColorStore.getColorByName("blue");
+        Color copy2 = ColorStore.getColorByName("blue");
 
+        System.out.println(copy1 == copy2); //output: false --> they're different objects even if same color
 
     }
 }
